5c3115d2fa1113238a4c5e8a8d76fcd4ccfbc928
sanjeev288 <81298748+sanjeev288@users.noreply.github.com>
2021-03-30 13:55:39 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express =require('express');
-const bodyParser = require('body-parser');
-
-const  { mangoose}= require('./db.js');
-var app=express();
-app.arguments(bodyParser.json());
-app.listen(3000,()=> console.log('server started at port:3000'));
